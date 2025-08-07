9ce3bf00039ce5cf85fc3157a02149e3207f232d
BearingMe <gomes.bruno201@gmail.com>
2025-08-07 15:21:31 -0300

    chore: update .gitignore and eslint configuration for better file management and formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from "./.nuxt/eslint.config.mjs";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
-export default withNuxt([eslintConfigPrettier]
-)
+export default withNuxt([
+  {
+    rules: {
+      "vue/multi-word-component-names": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+    },
+  },
+  eslintConfigPrettier,
+]);
