47ee2a527a2918eefd38966df11e903ebce9c3cc
BearingMe <gomes.bruno201@gmail.com>
2025-08-07 14:57:25 -0300

    chore: set up tailwind css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,15 @@
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  
+  compatibilityDate: "2025-07-15",
+
   devtools: { enabled: true },
 
+  css: ["~/assets/css/main.css"],
+
   vite: {
-    plugins: [
-      tailwindcss(),
-    ],
+    plugins: [tailwindcss()],
   },
 
-  modules: [
-    '@nuxt/icon',
-    '@pinia/nuxt',
-    '@vueuse/nuxt',
-    '@nuxt/eslint'
-  ]
-})
+  modules: ["@nuxt/icon", "@pinia/nuxt", "@vueuse/nuxt", "@nuxt/eslint"],
+});
