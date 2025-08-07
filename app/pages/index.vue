61b231c0c56166b10a7005a7877d1d20807b8a21
BearingMe <gomes.bruno201@gmail.com>
2025-08-07 16:20:58 -0300

    feat: add primary, secondary, and tertiary button components with customizable themes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,13 @@
+<script setup lang="ts">
+import ButtonPrimary from "~/volt/ButtonPrimary.vue";
+import ButtonSecondary from "~/volt/ButtonSecondary.vue";
+import ButtonTertiary from "~/volt/ButtonTertiary.vue";
+</script>
+
 <template>
-  <h1>Hello World!</h1>
+  <div class="flex flex-col w-[150px]">
+    <ButtonPrimary>Click Me</ButtonPrimary>
+    <ButtonSecondary>No, Click Me </ButtonSecondary>
+    <ButtonTertiary>Shut up</ButtonTertiary>
+  </div>
 </template>
