58de3df4c0502cadd8965f65c4393fc5ca76cab3
BearingMe <gomes.bruno201@gmail.com>
2025-12-25 16:30:32 -0300

    feat: add new Volt components and utilities for enhanced UI elements
    - Introduced Splitter, Step, StepItem, StepList, StepPanel, StepPanels, Stepper, Tab, TabList, TabPanel, TabPanels, Tag, Textarea, Timeline, Toast, ToggleButton, ToggleSwitch, Toolbar, and Tree components.
    - Implemented custom theming using PassThrough options for each component.
    - Created utility function `ptViewMerge` for merging PassThrough properties with Tailwind CSS support.
    - Removed deprecated ButtonSecondary component.

--- app/components/volt/utils.ts
+++ app/components/volt/utils.ts
@@ -2,18 +2,9 @@ import type { PassThroughMergePropsType } from "primevue/passthrough";
 import { twMerge } from "tailwind-merge";
 import { mergeProps } from "vue";
 
-export const ptViewMerge: PassThroughMergePropsType = (
-  globalPTProps = {},
-  selfPTProps = {},
-  datasets,
-) => {
+export const ptViewMerge: PassThroughMergePropsType = (globalPTProps = {}, selfPTProps = {}, datasets) => {
   const { class: globalClass, ...globalRest } = globalPTProps;
   const { class: selfClass, ...selfRest } = selfPTProps;
 
-  return mergeProps(
-    { class: twMerge(globalClass, selfClass) },
-    globalRest,
-    selfRest,
-    datasets,
-  );
+  return mergeProps({ class: twMerge(globalClass, selfClass) }, globalRest, selfRest, datasets);
 };
